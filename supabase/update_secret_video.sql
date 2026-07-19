6c2ab2ebbb5bdcb70b1ceb7f9cb12f8223bd855e
gardeauarbres <gard.eau.arbres@gmail.com>
2026-07-19 22:49:20 +0200

    feat: add video for secret playlist and sql script

---
+++ supabase/update_secret_video.sql
@@ -0,0 +1,4 @@
+-- Mise à jour pour afficher la vidéo en fond de carte au lieu de l'image
+UPDATE public.bonus_content
+SET image_url = '../assets/videos/play liste secrete1.mp4'
+WHERE title = 'Bonus Top Secret Ramonito 🌴';
