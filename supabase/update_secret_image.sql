a20ac140c5f04a7a4f23992ee0a8d0f6522abae9
gardeauarbres <gard.eau.arbres@gmail.com>
2026-07-19 21:22:44 +0200

    feat: add image and sql script for secret playlist

---
+++ supabase/update_secret_image.sql
@@ -0,0 +1,4 @@
+-- Mise à jour de l'image pour le Bonus Top Secret Ramonito
+UPDATE public.bonus_content
+SET image_url = '../assets/images/secret_playlist.jpg'
+WHERE title = 'Bonus Top Secret Ramonito 🌴';
